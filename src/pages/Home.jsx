f1d9ba1e1da4b8108d5a06d346321aaaf70df33b
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2024-06-13 16:27:47 +0000

    model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Loader from '../components/Loader'
+import { Island } from '../models/island'
 const Home = () => {
   return (
     <div>
@@ -13,7 +14,13 @@ const Home = () => {
 
         <Canvas className='w-full h-screen bg-transparent' camera={{near:0.1,far:1000}}>
           <Suspense fallback={<Loader/>}>
-
+            <directionalLight/>
+            <ambientLight/>
+            <pointLight/>
+            <spotLight/>
+            <hemisphereLight/>
+            
+            <Island/>
           </Suspense>
         </Canvas>
       </section>
